6b9df2b247456068929eefa7c3a717d9e9a569bd
AlexanderSergeev <asergeev1997@yandex.ru>
2016-11-29 16:36:52 +0300

    Test versions of edit and insert for abiturient

--- src/backend/ModelDBConnection.java
+++ src/backend/ModelDBConnection.java
@@ -12,28 +12,29 @@ public class ModelDBConnection {
 	static String serverAddress;
 	static String serverType;
 	static String dbName;
-	
-    static Connection con=null;
-    static CallableStatement cstmt=null;
-    static ResultSet rset=null;
-    static Statement stmt=null;
-	
-	public static void setConnectionParameters(String serverType, String serverAddress, String dbName, String login, String password) {
+
+	static Connection con = null;
+	static CallableStatement cstmt = null;
+	static ResultSet rset = null;
+	static Statement stmt = null;
+
+	public static void setConnectionParameters(String serverType, String serverAddress, String dbName, String login,
+			String password) {
 		ModelDBConnection.serverType = serverType;
 		ModelDBConnection.serverAddress = serverAddress;
 		ModelDBConnection.dbName = dbName;
 		ModelDBConnection.login = login;
 		ModelDBConnection.password = password;
 	}
-	
+
 	public String getLogin() {
 		return login;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
-	
+
 	public String getServerAddress() {
 		return serverAddress;
 	}
@@ -47,20 +48,23 @@ public class ModelDBConnection {
 	}
 
 	public static boolean initConnection() {
-		if(con==null) {
+		if (con == null) {
 			try {
 				String connectionUrl = null;
 				switch (serverType) {
-					case "AzureServer":
-						connectionUrl="jdbc:sqlserver://" + serverAddress + ":1433;database=" + dbName + ";user=" + login + "@projectownserver;password=" + password + ";encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
-						break;
-					case "MSServer":
-						connectionUrl = "jdbc:sqlserver://" + serverAddress + ":1433;databaseName=" + dbName + ";user=" + login + ";password=" + password + ";";
-						break;
+				case "AzureServer":
+					connectionUrl = "jdbc:sqlserver://" + serverAddress + ":1433;database=" + dbName + ";user=" + login
+							+ "@projectownserver;password=" + password
+							+ ";encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+					break;
+				case "MSServer":
+					connectionUrl = "jdbc:sqlserver://" + serverAddress + ":1433;databaseName=" + dbName + ";user="
+							+ login + ";password=" + password + ";";
+					break;
 				}
-				
-	    		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-	    		con = DriverManager.getConnection(connectionUrl);
+
+				Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+				con = DriverManager.getConnection(connectionUrl);
 			} catch (Exception e) {
 				e.printStackTrace();
 				return false;
@@ -68,59 +72,129 @@ public class ModelDBConnection {
 		}
 		return true;
 	}
-	
+
 	public static int getCountAbiturients() {
-		if(initConnection()) {
-    		try {
-    			int countAbits = 0;
+		if (initConnection()) {
+			try {
+				int countAbits = 0;
 				String query = "select count(*) from Abiturient";
-	    		stmt = con.createStatement();
-	    		rset = stmt.executeQuery(query);
+				stmt = con.createStatement();
+				rset = stmt.executeQuery(query);
 
-        		while (rset.next()) {
-        			countAbits = rset.getInt(1);
-	    		}
+				while (rset.next()) {
+					countAbits = rset.getInt(1);
+				}
 
-        		stmt.close();
-        		rset.close();
+				stmt.close();
+				rset.close();
 
-        		return countAbits;
-    		} catch (Exception e) {
-    			e.printStackTrace();
-    			return -1;
-    		}
+				return countAbits;
+			} catch (Exception e) {
+				e.printStackTrace();
+				return -1;
+			}
 		}
 		return -1;
 	}
-	
+
 	public static String[][] getAllAbiturients() {
 		int countAbits = getCountAbiturients();
-		
-		String [][] data = null;
-		
+
+		String[][] data = null;
+
 		if (countAbits > 0) {
-    		try {
+			try {
 				data = new String[countAbits][4];
-				
+
 				String query = "select aid, SName, Fname, MName from Abiturient order by aid";
 				stmt = con.createStatement();
 				rset = stmt.executeQuery(query);
-				
-				int curPos = 0;
-        		while (rset.next()) {
-        			data[curPos][0] = String.valueOf(rset.getInt(1));
-        			data[curPos][1] = rset.getString(2);
-        			data[curPos][2] = rset.getString(3);
-        			data[curPos][3] = rset.getString(4);
-	    		}
 
-        		stmt.close();
-        		rset.close();
-    		} catch (Exception e) {
-    			e.printStackTrace();
-    			return null;
-    		}
+				int curPos = 0;
+				while (rset.next()) {
+					data[curPos][0] = String.valueOf(rset.getInt(1));
+					data[curPos][1] = rset.getString(2);
+					data[curPos][2] = rset.getString(3);
+					data[curPos][3] = rset.getString(4);
+				}
+
+				stmt.close();
+				rset.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
 		}
 		return data;
+	}
+
+	// TODO test
+	public static void insertAbiturient(String[] info) {
+		String aid = info[0];
+		String SName = "'" + info[1] + "'";
+		String FName = "'" + info[2] + "'";
+		String MName = "'" + info[3] + "'";
+		String birthday = "'" + info[4] + "'";
+		String birthplace = "'" + info[5] + "'";
+		String id_gender = info[6];
+		String id_nationality = info[7];
+		String email = "'" + info[8] + "'";
+		String phoneNumbers = "'" + info[9] + "'";
+		String needHostel = info[10];
+		String registrationDate = "'" + info[11] + "'";
+		String returnDate = "'" + info[12] + "'";
+		String id_returnReason = info[13];
+		String needSpecConditions = info[14];
+		String is_enrolled = info[15];
+
+		try {
+			String query = "insert into Abiturient Values (" + aid + ", " + SName + ", " + FName + ", " + MName + ", "
+					+ birthday + ", " + birthplace + ", " + id_gender + ", " + id_nationality + ", " + email + ", "
+					+ phoneNumbers + ", " + needHostel + ", " + registrationDate + ", " + returnDate + ", "
+					+ id_returnReason + ", " + needSpecConditions + ", " + is_enrolled + ");";
+			stmt = con.createStatement();
+			rset = stmt.executeQuery(query);
+
+			stmt.close();
+			rset.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// TODO test
+	public static void editAbiturient(String[] info) {
+		String aid = info[0];
+		String SName = "'" + info[1] + "'";
+		String FName = "'" + info[2] + "'";
+		String MName = "'" + info[3] + "'";
+		String Birthday = "'" + info[4] + "'";
+		String Birthplace = "'" + info[5] + "'";
+		String id_gender = info[6];
+		String id_nationality = info[7];
+		String email = "'" + info[8] + "'";
+		String phoneNumbers = "'" + info[9] + "'";
+		String needHostel = info[10];
+		String registrationDate = "'" + info[11] + "'";
+		String returnDate = "'" + info[12] + "'";
+		String id_returnReason = info[13];
+		String needSpecConditions = info[14];
+		String is_enrolled = info[15];
+
+		try {
+			String query = "update Abiturient set SName = " + SName + ", FName = " + FName + ", MName = " + MName
+					+ ", Birthday = " + Birthday + ", Birthplace = " + Birthplace + ", id_gender = " + id_gender
+					+ ", id_nationality = " + id_nationality + ", email = " + email + ", phoneNumbers = " + phoneNumbers
+					+ ", needHostel = " + needHostel + ", registrationDate = " + registrationDate + ", returnDate = "
+					+ returnDate + ", id_returnReason = " + id_returnReason + ", needSpecConditions = "
+					+ needSpecConditions + ", is_enrolled = " + is_enrolled + " where aid = " + aid + ";";
+			stmt = con.createStatement();
+			rset = stmt.executeQuery(query);
+
+			stmt.close();
+			rset.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/firstTest/TestConnection.java
+++ src/firstTest/TestConnection.java
@@ -8,43 +8,61 @@ import java.sql.*;
 public class TestConnection {
 
 	public static void main(String[] args) {
-		
+
 		// Create a variable for the connection string.
 		// TODO edit it!
-		String connectionUrl ="jdbc:sqlserver://projectownserver.database.windows.net:1433;database=Project;user=Alexander@projectownserver;password={password here!};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;"; 
+		String connectionUrl = "jdbc:sqlserver://projectownserver.database.windows.net:1433;database=Project;user=Alexander@projectownserver;password={password here!};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
 
 		// Declare the JDBC objects.
 		Connection con = null;
 		Statement stmt = null;
 		ResultSet rs = null;
-		
-        	try {
-        		// Establish the connection.
-        		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            		con = DriverManager.getConnection(connectionUrl);
-            
-            		// Create and execute an SQL statement that returns some data.
-            		//String SQL = "SELECT * FROM Abiturient";
-            		String SQL = "CREATE TABLE Abiturient (aid int primary key, SName text, Fname text,MName text)";
-            		stmt = con.createStatement();
-            		//rs = stmt.executeQuery(SQL);
-            		stmt.executeUpdate(SQL);
-            		
-            		// Iterate through the data in the result set and display it.
-            		//while (rs.next()) {
-            			//System.out.println(rs.getString(1));
-            		//}
-        	}
-        
+
+		try {
+			// Establish the connection.
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			con = DriverManager.getConnection(connectionUrl);
+
+			// Create and execute an SQL statement that returns some data.
+			// String SQL = "SELECT * FROM Abiturient";
+			String SQL = "CREATE TABLE Abiturient (aid int primary key, SName text, Fname text, MName text)";
+			stmt = con.createStatement();
+			// rs = stmt.executeQuery(SQL);
+			stmt.executeUpdate(SQL);
+
+			/*
+			 * String[] info = { "1", "Smith", "John", "Vladimirovich",
+			 * "25-04-1885", "Nevada", "1", "1", "xxx@mail.ru", "5-83-53", "0",
+			 * "07-08-2015", "", "0", "0", "0" };
+			 * ModelDBConnection.insertAbiturient(info);
+			 */
+			// Iterate through the data in the result set and display it.
+			// while (rs.next()) {
+			// System.out.println(rs.getString(1));
+			// }
+		}
+
 		// Handle any errors that may have occurred.
 		catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		finally {
-			if (rs != null) try { rs.close(); } catch(Exception e) {}
-	   		if (stmt != null) try { stmt.close(); } catch(Exception e) {}
-	   		if (con != null) try { con.close(); } catch(Exception e) {}
+			if (rs != null)
+				try {
+					rs.close();
+				} catch (Exception e) {
+				}
+			if (stmt != null)
+				try {
+					stmt.close();
+				} catch (Exception e) {
+				}
+			if (con != null)
+				try {
+					con.close();
+				} catch (Exception e) {
+				}
 		}
 	}
 }
